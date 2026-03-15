5325ef60df9b1f13cc39d0dfdadce853253429a9
tluke900 <luke.tomlinson@slu.edu>
2026-03-15 13:44:56 -0500

    End of Chapter 1
    Finished Chapter 1, included basic structure of COBOL and COBOL 85 as well as how to write a display command.

---
+++ COBOL 85/Hello World.cbl
@@ -0,0 +1,10 @@
+      $set dialect(ANS85)
+       Identification Division.
+       Program-Id. HELLOWLD.
+
+       Procedure Division.
+       100-Program.
+           Display 'Hello, World!'
+           Stop Run
+           .
+           
